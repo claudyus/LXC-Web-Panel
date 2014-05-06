5f3d293a42b23c841a913cb027225431e0ad5251
Claudio <c.mignanti@gmail.com>
2014-05-06 11:17:58 +0200

    fix: lint the code of lxclite class

--- lxclite/__init__.py
+++ lxclite/__init__.py
@@ -22,6 +22,7 @@ import time
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
 def _run(cmd, output=False):
     '''
     To run command easier
@@ -32,7 +33,7 @@ def _run(cmd, output=False):
         except subprocess.CalledProcessError:
             out = False
         return out
-    return subprocess.check_call('{}'.format(cmd), shell=True) # returns 0 for True
+    return subprocess.check_call('{}'.format(cmd), shell=True)  # returns 0 for True
 
 class ContainerAlreadyExists(Exception): pass
 class ContainerDoesntExists(Exception): pass
@@ -41,18 +42,22 @@ class ContainerNotRunning(Exception): pass
 class DirectoryDoesntExists(Exception): pass
 class NFSDirectoryNotMounted(Exception): pass
 
+
 def exists(container):
     '''
     Check if container exists
     '''
-    if container in ls(): return True
+    if container in ls():
+        return True
     return False
+
 
 def get_ipv4(container):
     '''
     Get the IPv4 address for a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exist!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exist!'.format(container))
     output = _run('lxc-ls --fancy {}'.format(container), output=True).splitlines()
     for l in output:
         parts = l.split()
@@ -60,17 +65,21 @@ def get_ipv4(container):
             return parts[2]
     return None
 
+
 def create(container, template='ubuntu', storage=None, xargs=None):
     '''
     Create a container (without all options)
     Default template: Ubuntu
     '''
-    if exists(container): raise ContainerAlreadyExists('Container {} already created!'.format(container))
+    if exists(container):
+        raise ContainerAlreadyExists('Container {} already created!'.format(container))
 
     command = 'lxc-create -n {}'.format(container)
     command += ' -t {}'.format(template)
-    if storage: command += ' -B {}'.format(storage)
-    if xargs: command += ' -- {}'.format(xargs)
+    if storage:
+        command += ' -B {}'.format(storage)
+    if xargs:
+        command += ' -- {}'.format(xargs)
 
     return _run(command)
 
@@ -80,7 +89,8 @@ def clone(orig=None, new=None, snapshot=False):
     Clone a container (without all options)
     '''
     if orig and new:
-        if exists(new): raise ContainerAlreadyExists('Container {} already exist!'.format(new))
+        if exists(new):
+            raise ContainerAlreadyExists('Container {} already exist!'.format(new))
 
         if snapshot:
             command = 'lxc-clone -s {} {}'.format(orig, new)
@@ -94,7 +104,8 @@ def info(container):
     '''
     Check info from lxc-info
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exist!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exist!'.format(container))
 
     output = _run('lxc-info -qn {}'.format(container), output=True).splitlines()
     
@@ -119,13 +130,13 @@ def ls():
     lxcdir = '/var/lib/lxc/'
     ct_list = []
 
-    try: 
-	lsdir  = os.listdir(lxcdir)
-	for i in lsdir:
-    	    if os.path.isdir(lxcdir+i):
-    		ct_list.append(i)
-    except OSError: 
-	ct_list = []
+    try:
+        lsdir = os.listdir(lxcdir)
+        for i in lsdir:
+            if os.path.isdir(lxcdir + i):
+                ct_list.append(i)
+    except OSError:
+        ct_list = []
     return sorted(ct_list)
 
 
@@ -168,8 +179,10 @@ def start(container):
     '''
     Starts a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exists!'.format(container))
-    if container in running(): raise ContainerAlreadyRunning('Container {} is already running!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exists!'.format(container))
+    if container in running():
+        raise ContainerAlreadyRunning('Container {} is already running!'.format(container))
     return _run('lxc-start -dn {}'.format(container))
 
 
@@ -177,8 +190,10 @@ def stop(container):
     '''
     Stops a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exists!'.format(container))
-    if container in stopped(): raise ContainerNotRunning('Container {} is not running!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exists!'.format(container))
+    if container in stopped():
+        raise ContainerNotRunning('Container {} is not running!'.format(container))
     return _run('lxc-stop -n {}'.format(container))
 
 
@@ -186,8 +201,10 @@ def freeze(container):
     '''
     Freezes a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exists!'.format(container))
-    if not container in running(): raise ContainerNotRunning('Container {} is not running!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exists!'.format(container))
+    if not container in running():
+        raise ContainerNotRunning('Container {} is not running!'.format(container))
     return _run('lxc-freeze -n {}'.format(container))
 
 
@@ -195,8 +212,10 @@ def unfreeze(container):
     '''
     Unfreezes a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exists!'.format(container))
-    if not container in frozen(): raise ContainerNotRunning('Container {} is not frozen!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exists!'.format(container))
+    if not container in frozen():
+        raise ContainerNotRunning('Container {} is not frozen!'.format(container))
     return _run('lxc-unfreeze -n {}'.format(container))
 
 
@@ -204,7 +223,8 @@ def destroy(container):
     '''
     Destroys a container
     '''
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exists!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exists!'.format(container))
     return _run('lxc-destroy -n {}'.format(container))
 
 
@@ -213,12 +233,16 @@ def checkconfig():
     Returns the output of lxc-checkconfig (colors cleared)
     '''
     out = _run('lxc-checkconfig', output=True)
-    if out: return out.replace('[1;32m', '').replace('[1;33m', '').replace('[0;39m', '').replace('[1;32m', '').replace('\x1b', '').replace(': ', ':').split('\n')
+    if out:
+        return out.replace('[1;32m', '').replace('[1;33m', '').replace('[0;39m', '').replace('[1;32m', '').replace('\x1b', '').replace(': ', ':').split('\n')
     return out
 
+
 def cgroup(container, key, value):
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exist!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exist!'.format(container))
     return _run('lxc-cgroup -n {} {} {}'.format(container, key, value))
+
 
 def backup(container, sr_type='local', destination='/var/lxc-backup/'):
     '''
@@ -230,19 +254,22 @@ def backup(container, sr_type='local', destination='/var/lxc-backup/'):
     filename = '{}/{}-{}'.format(destination, container, prefix)
     was_running = False
 
-    if not exists(container): raise ContainerDoesntExists('Container {} does not exist!'.format(container))
+    if not exists(container):
+        raise ContainerDoesntExists('Container {} does not exist!'.format(container))
     if sr_type == 'local':
-    	if not os.path.isdir(destination): raise DirectoryDoesntExists('Directory {} does not exist !'.format(destination))
+        if not os.path.isdir(destination):
+            raise DirectoryDoesntExists('Directory {} does not exist !'.format(destination))
     if sr_type == 'nfs':
-        if not os.path.ismount(destination): raise NFSDirectoryNotMounted('NFS {} is not mounted !'.format(destination))
+        if not os.path.ismount(destination):
+            raise NFSDirectoryNotMounted('NFS {} is not mounted !'.format(destination))
 
     if info(container)['state'] == 'RUNNING':
         was_running = True
         freeze(container)
 
-    create_backup  = _run('tar czf {} -C /var/lib/lxc {}'.format(filename, container))
+    create_backup = _run('tar czf {} -C /var/lib/lxc {}'.format(filename, container))
 
-    if was_running == True:
+    if was_running is True:
         unfreeze(container)
 
     return create_backup
