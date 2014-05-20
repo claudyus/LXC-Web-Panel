eef518ce2825e296c96dbeca354213607fc3e4cd
Claudio <c.mignanti@gmail.com>
2014-05-20 15:22:00 +0200

    remove md5sum ajax hack

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
 all:
 	python rst2html.py ../README.rst > site-src/pages/README.html
 	cat ../CHANGELOG > site-src/pages/CHANGELOG.html
-	md5sum *.deb > site-src/static/md5sum
-	python md52json.py site-src/static/md5sum > site-src/static/md5sum.json
+	md5sum *.deb > md5sum
 	#build after any site-src modification
 	cd site-src; cactus build; cd ..
 	cp -r site-src/.build/* .
--- site-src/plugins/page_context.py
+++ site-src/plugins/page_context.py
@@ -1,5 +1,8 @@
 #coding:utf-8
 
+import os
+
+
 def preBuildPage(page, context, data):
     """
     Updates the context of the page to include: the page itself as {{ CURRENT_PAGE }}
@@ -8,9 +11,17 @@ def preBuildPage(page, context, data):
     # This will run for each page that Cactus renders.
     # Any changes you make to context will be passed to the template renderer for this page.
 
+    md5 = open(os.getcwd() + '/../md5sum', 'r')
+    arr = []
+    for line in md5:
+        parts = line.split()
+        dic = {'file': parts[1], 'md5': parts[0]}
+        arr.append(dic)
+
     extra = {
-        "CURRENT_PAGE": page
+        "CURRENT_PAGE": page,
         # Add your own dynamic context elements here!
+        "released_files": arr
     }
 
     context.update(extra)
