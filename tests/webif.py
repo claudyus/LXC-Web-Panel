88b46a9f8f7f315536cf0a99ff82be83884d291f
Claudio Mignanti <c.mignanti@unidata.it>
2015-01-26 21:39:46 +0100

    tests: implement some apis test

--- tests/webif.py
+++ tests/webif.py
@@ -1,9 +1,13 @@
 import unittest
 import urllib2
+import shutil
+import json
+
 from flask import Flask
 from flask.ext.testing import LiveServerTestCase
 
 from lwp.app import app
+from lwp.utils import connect_db
 
 class TestWebIf(LiveServerTestCase):
 
@@ -21,9 +25,58 @@ class TestWebIf(LiveServerTestCase):
             '/lwp/tokens', '/action', '/_refresh_info', '/_check_version')
 
         for link in endpoints:
-            response = urllib2.urlopen(self.get_server_url()+link)
+            response = urllib2.urlopen(self.get_server_url() + link)
             self.assertEqual(response.code, 200)
 
+class TestApi(LiveServerTestCase):
+
+    token = 'myrandomapitestoken'
+    db = None
+
+    def create_app(self):
+        shutil.copyfile('lwp.db', '/tmp/db.sql')
+        self.db = connect_db('/tmp/db.sql')
+        self.db.execute('insert into api_tokens(description, token) values(?, ?)', ['test', self.token])
+        self.db.commit()
+        app.config['DATABASE'] = '/tmp/db.sql'
+        return app
+
+    def setUp(self):
+        shutil.copyfile('lwp.db', '/tmp/db.sql')
+        self.db = connect_db('/tmp/db.sql')
+        self.db.execute('insert into api_tokens(description, token) values(?, ?)', ['test', self.token])
+        self.db.commit()
+
+    def test_get_containers(self):
+        q = urllib2.Request(self.get_server_url() + '/api/v1/containers/')
+        q.add_header('Private-Token', self.token)
+        response = urllib2.urlopen(q)
+        self.assertEqual(response.code, 200)
+        #assert isinstance(response.read(), list)
+
+    # def test_put_containers(self):
+    #     data = {'name': 'test_vm_sshd', 'template': 'sshd'}
+    #     q = urllib2.Request(self.get_server_url() + '/api/v1/containers/', json.dumps(data))
+    #     q.add_header('Private-Token', self.token)
+    #     q.get_method = lambda: 'PUT'
+    #     response = urllib2.urlopen(q)
+    #     self.assertEqual(response.code, 200)
+
+    # TODO: implement apis tests
+
+    def test_post_token(self):
+        data = {'token': 'test'}
+        q = urllib2.Request(self.get_server_url() + '/api/v1/tokens/', json.dumps(data))
+        q.add_header('Private-Token', self.token)
+        response = urllib2.urlopen(q)
+        self.assertEqual(response.code, 200)
+
+    def test_delete_token(self):
+        q = urllib2.Request(self.get_server_url() + '/api/v1/tokens/test')
+        q.add_header('Private-Token', self.token)
+        q.get_method = lambda: 'DELETE'
+        response = urllib2.urlopen(q)
+        self.assertEqual(response.code, 200)
 
 if __name__ == '__main__':
     unittest.main()
