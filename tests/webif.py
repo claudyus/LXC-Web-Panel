7a143b21f85d7d1c295a09bf9aa4cd70ff8c3072
Claudio Mignanti <c.mignanti@gmail.com>
2015-01-27 22:07:22 +0100

    Test the lxc mock scripts

--- tests/webif.py
+++ tests/webif.py
@@ -1,3 +1,4 @@
+import subprocess
 import unittest
 import urllib2
 import shutil
@@ -88,6 +89,50 @@ class TestApi(LiveServerTestCase):
         request.get_method = lambda: 'DELETE'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
+
+
+class TestMockLxc(unittest.TestCase):
+
+    """
+    Those tests are against the lxc mock system under tests/mock-lxc/
+
+    To use those tests on your machine with lxc installed you should
+     add the path in your ENV before the default one:
+        export PATH=`pwd`/tests/mock-lxc/:$PATH
+    """
+
+    def test_01_config(self):
+        shutil.rmtree('/tmp/lxc', ignore_errors=True)
+        out = subprocess.check_output('lxc-config lxc.lxcpath', shell=True, close_fds=True).strip()
+        assert out == '/tmp/lxc'
+
+    def test_02_create(self):
+        subprocess.check_output('lxc-create -n lxctest', shell=True, close_fds=True).strip()
+        assert 'lxctest' in os.listdir('/tmp/lxc')
+        with open('/tmp/lxc/lxctest/status') as f:
+            content = f.readlines()
+            assert 'STOPPED' in content[0].rstrip('\n')
+
+    def test_03_start(self):
+        subprocess.check_output('lxc-start -dn lxctest', shell=True, close_fds=True).strip()
+        with open('/tmp/lxc/lxctest/status') as f:
+            content = f.readlines()
+            assert 'RUNNING' in content[0].rstrip('\n')
+
+    def test_04_ls(self):
+        out = subprocess.check_output('lxc-ls --fancy | grep test', shell=True, close_fds=True).strip()
+        assert 'lxctest' in out
+
+    def test_05_stop(self):
+        subprocess.check_output('lxc-stop -n lxctest', shell=True, close_fds=True).strip()
+        with open('/tmp/lxc/lxctest/status') as f:
+            content = f.readlines()
+            assert 'STOPPED' in content[0].rstrip('\n')
+
+    def test_06_destroy(self):
+        subprocess.check_output('lxc-destroy -n lxctest', shell=True, close_fds=True).strip()
+        assert 'lxctest' not in os.listdir('/tmp/lxc')
+
 
 if __name__ == '__main__':
     unittest.main()
