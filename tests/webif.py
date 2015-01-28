88e437add2bdf0065dd803ef3f58bdaa29c387d1
Claudio Mignanti <c.mignanti@gmail.com>
2015-01-28 21:59:14 +0100

    test: refactor previous test with common token, remove closefds in subprocess

--- tests/webif.py
+++ tests/webif.py
@@ -79,14 +79,13 @@ class TestWebIf(LiveServerTestCase):
 
 class TestApi(LiveServerTestCase):
 
-    token = 'myrandomapitestoken'
     db = None
     type_json = {'Content-Type': 'application/json'}
 
     def create_app(self):
         shutil.copyfile('lwp.db', '/tmp/db.sql')
         self.db = connect_db('/tmp/db.sql')
-        self.db.execute('insert into api_tokens(description, token) values(?, ?)', ['test', self.token])
+        self.db.execute('insert into api_tokens(description, token) values(?, ?)', ['test', token])
         self.db.commit()
         app.config['DATABASE'] = '/tmp/db.sql'
         return app
@@ -94,7 +93,7 @@ class TestApi(LiveServerTestCase):
     def test_00_get_containers(self):
         shutil.rmtree('/tmp/lxc/', ignore_errors=True)
         request = urllib2.Request(self.get_server_url() + '/api/v1/containers/')
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
         #assert isinstance(response.read(), list)
@@ -102,7 +101,7 @@ class TestApi(LiveServerTestCase):
     def test_01_put_containers(self):
         data = {'name': 'test_vm_sshd', 'template': 'sshd'}
         request = urllib2.Request(self.get_server_url() + '/api/v1/containers/', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         request.get_method = lambda: 'PUT'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
@@ -111,14 +110,14 @@ class TestApi(LiveServerTestCase):
     def test_02_post_containers(self):
         data = {'action': 'start'}
         request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         request.get_method = lambda: 'POST'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
 
     def test_03_delete_containers(self):
         request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd')
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         request.get_method = lambda: 'DELETE'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
@@ -126,13 +125,13 @@ class TestApi(LiveServerTestCase):
     def test_04_post_token(self):
         data = {'token': 'test'}
         request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
 
     def test_05_delete_token(self):
         request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/test')
-        request.add_header('Private-Token', self.token)
+        request.add_header('Private-Token', token)
         request.get_method = lambda: 'DELETE'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
@@ -150,34 +149,34 @@ class TestMockLxc(unittest.TestCase):
 
     def test_01_config(self):
         shutil.rmtree('/tmp/lxc', ignore_errors=True)
-        out = subprocess.check_output('lxc-config lxc.lxcpath', shell=True, close_fds=True).strip()
+        out = subprocess.check_output('lxc-config lxc.lxcpath', shell=True).strip()
         assert out == '/tmp/lxc'
 
     def test_02_create(self):
-        subprocess.check_output('lxc-create -n lxctest', shell=True, close_fds=True).strip()
+        subprocess.check_output('lxc-create -n lxctest', shell=True).strip()
         assert 'lxctest' in os.listdir('/tmp/lxc')
         with open('/tmp/lxc/lxctest/status') as f:
             content = f.readlines()
             assert 'STOPPED' in content[0].rstrip('\n')
 
     def test_03_start(self):
-        subprocess.check_output('lxc-start -dn lxctest', shell=True, close_fds=True).strip()
+        subprocess.check_output('lxc-start -dn lxctest', shell=True).strip()
         with open('/tmp/lxc/lxctest/status') as f:
             content = f.readlines()
             assert 'RUNNING' in content[0].rstrip('\n')
 
     def test_04_ls(self):
-        out = subprocess.check_output('lxc-ls --fancy | grep test', shell=True, close_fds=True).strip()
+        out = subprocess.check_output('lxc-ls --fancy | grep test', shell=True).strip()
         assert 'lxctest' in out
 
     def test_05_stop(self):
-        subprocess.check_output('lxc-stop -n lxctest', shell=True, close_fds=True).strip()
+        subprocess.check_output('lxc-stop -n lxctest', shell=True).strip()
         with open('/tmp/lxc/lxctest/status') as f:
             content = f.readlines()
             assert 'STOPPED' in content[0].rstrip('\n')
 
     def test_06_destroy(self):
-        subprocess.check_output('lxc-destroy -n lxctest', shell=True, close_fds=True).strip()
+        subprocess.check_output('lxc-destroy -n lxctest', shell=True).strip()
         assert 'lxctest' not in os.listdir('/tmp/lxc')
 
 
