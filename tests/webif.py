aa0aae379ed59823fb4651ac9e37064c135ad1a5
Claudio Mignanti <c.mignanti@gmail.com>
2015-02-14 15:43:04 +0100

    move mock_lxc test on separate file

--- tests/webif.py
+++ tests/webif.py
@@ -137,48 +137,5 @@ class TestApi(LiveServerTestCase):
         self.assertEqual(response.code, 200)
 
 
-class TestMockLxc(unittest.TestCase):
-
-    """
-    Those tests are against the lxc mock system under tests/mock-lxc/
-
-    To use those tests on your machine with lxc installed you should
-     add the path in your ENV before the default one:
-        export PATH=`pwd`/tests/mock-lxc/:$PATH
-    """
-
-    def test_01_config(self):
-        shutil.rmtree('/tmp/lxc', ignore_errors=True)
-        out = subprocess.check_output('lxc-config lxc.lxcpath', shell=True).strip()
-        assert out == '/tmp/lxc'
-
-    def test_02_create(self):
-        subprocess.check_output('lxc-create -n lxctest', shell=True).strip()
-        assert 'lxctest' in os.listdir('/tmp/lxc')
-        with open('/tmp/lxc/lxctest/status') as f:
-            content = f.readlines()
-            assert 'STOPPED' in content[0].rstrip('\n')
-
-    def test_03_start(self):
-        subprocess.check_output('lxc-start -dn lxctest', shell=True).strip()
-        with open('/tmp/lxc/lxctest/status') as f:
-            content = f.readlines()
-            assert 'RUNNING' in content[0].rstrip('\n')
-
-    def test_04_ls(self):
-        out = subprocess.check_output('lxc-ls --fancy | grep test', shell=True).strip()
-        assert 'lxctest' in out
-
-    def test_05_stop(self):
-        subprocess.check_output('lxc-stop -n lxctest', shell=True).strip()
-        with open('/tmp/lxc/lxctest/status') as f:
-            content = f.readlines()
-            assert 'STOPPED' in content[0].rstrip('\n')
-
-    def test_06_destroy(self):
-        subprocess.check_output('lxc-destroy -n lxctest', shell=True).strip()
-        assert 'lxctest' not in os.listdir('/tmp/lxc')
-
-
 if __name__ == '__main__':
     unittest.main()
