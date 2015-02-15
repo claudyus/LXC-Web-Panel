9e4015d64032412411678bc5354e9d994738f86c
Lukáš Raška <lukas@raska.me>
2015-02-15 18:20:55 +0100

    [misc] fix intendation + case-sensitive variables

--- lwp/authenticators/stub.py
+++ lwp/authenticators/stub.py
@@ -2,6 +2,7 @@
 # Example authenticator.
 # Class has to have the same name as file (case sensitive) and implement authenticate(self, username, password) method.
 
+
 class stub:
     def authenticate(self, username, password):
-        return true
+        return True
