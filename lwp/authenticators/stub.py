c9d4abdeb8b7aae326410f034129ccb16bb5c390
Lukáš Raška <lukas@raska.me>
2015-02-15 17:52:51 +0100

    [misc] Update readme with latest updates

---
+++ lwp/authenticators/stub.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Example authenticator.
+# Class has to have the same name as file (case sensitive) and implement authenticate(self, username, password) method.
+
+class stub:
+    def authenticate(self, username, password):
+        return true
