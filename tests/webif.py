851b3fad92f4289f57deb398551064802403b276
Claudio Mignanti <c.mignanti@gmail.com>
2015-03-31 21:27:52 +0200

    fix tests

--- tests/webif.py
+++ tests/webif.py
@@ -44,8 +44,8 @@ class TestWebIf(LiveServerTestCase):
     def test_home_rendering(self):
         subprocess.check_output('lxc-create -n mocktest_00_lxc', shell=True)
 
-        request = urllib2.Request(self.get_server_url() + '/home')
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/home',
+            headers={'Private-Token': token})
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
         assert response.read().find('mocktest_00_lxc')
@@ -53,8 +53,8 @@ class TestWebIf(LiveServerTestCase):
     def test_refresh_info(self):
         subprocess.check_output('lxc-create -n mocktest', shell=True)
 
-        request = urllib2.Request(self.get_server_url() + '/_refresh_info')
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/_refresh_info',
+            headers={'Private-Token': token})
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
         j_data = response.read()
@@ -92,16 +92,16 @@ class TestApi(LiveServerTestCase):
 
     def test_00_get_containers(self):
         shutil.rmtree('/tmp/lxc/', ignore_errors=True)
-        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/')
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/',
+            headers={'Private-Token': token})
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
         #assert isinstance(response.read(), list)
 
     def test_01_put_containers(self):
         data = {'name': 'test_vm_sshd', 'template': 'sshd'}
-        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/', json.dumps(data),
+            headers={'Private-Token': token, 'Content-Type': 'application/json' })
         request.get_method = lambda: 'PUT'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
@@ -109,29 +109,29 @@ class TestApi(LiveServerTestCase):
 
     def test_02_post_containers(self):
         data = {'action': 'start'}
-        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd', json.dumps(data),
+            headers={'Private-Token': token, 'Content-Type': 'application/json'})
         request.get_method = lambda: 'POST'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
 
     def test_03_delete_containers(self):
-        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd')
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/containers/test_vm_sshd',
+            headers={'Private-Token': token})
         request.get_method = lambda: 'DELETE'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
 
     def test_04_post_token(self):
         data = {'token': 'test'}
-        request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/', json.dumps(data), self.type_json)
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/', json.dumps(data),
+            headers={'Private-Token': token, 'Content-Type': 'application/json'})
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
 
     def test_05_delete_token(self):
-        request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/test')
-        request.add_header('Private-Token', token)
+        request = urllib2.Request(self.get_server_url() + '/api/v1/tokens/test',
+            headers={'Private-Token': token})
         request.get_method = lambda: 'DELETE'
         response = urllib2.urlopen(request)
         self.assertEqual(response.code, 200)
