d86ff7280cf905f3b13b7897e87a5fb5ceef7c50
Claudio Mignanti <c.mignanti@gmail.com>
2015-03-31 21:39:32 +0200

    add test cmdline

---
+++ tests/utils.py
@@ -0,0 +1,19 @@
+import subprocess
+import unittest
+import os
+
+
+class TestCmdLine(unittest.TestCase):
+
+    """
+    Those tests are against the lwp command lines
+    """
+
+    def test_generate_secret(self):
+        assert not os.path.exists('/etc/lwp/session_secret')
+        subprocess.check_output('python bin/lwp --generate-session-secret', shell=True, close_fds=True).strip()
+        assert os.path.exists('/etc/lwp/session_secret')
+
+
+if __name__ == '__main__':
+    unittest.main()
